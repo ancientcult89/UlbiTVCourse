25dcc1e61dad7a005a17a0708695e811ca8301d6
PolyakovSergey <ancientcult89@gmail.com>
2024-06-04 19:44:15 +0700

    lesson 11

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,18 @@
-import {Link, Route, Routes} from "react-router-dom";
+import {Link} from "react-router-dom";
 import './styles/index.scss'
-import {Suspense} from "react";
 import {classNames} from "shared/lib/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
+import {AppRouter} from "app/providers/router";
+import {Navbar} from "widget/NavBar";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
+            <Navbar/>
+            <AppRouter/>
             <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage/>}/>
-                    <Route path={'/'} element={<MainPage/>}/>
-                </Routes>
-            </Suspense>
         </div>
     );
 };
---
+++ src/widget/NavBar/ui/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+export const Navbar = () => {
+    return (
+        <div>
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+        </div>
+    );
+};
+
+export default Navbar;

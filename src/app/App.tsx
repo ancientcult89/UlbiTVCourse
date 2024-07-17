39425d99059e9db48d53bc0015fd576767e5d240
PolyakovSergey <ancientcult89@gmail.com>
2024-07-17 19:27:27 +0700

    lesson 29 22:44

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,17 +4,23 @@ import {useTheme} from "app/providers/ThemeProvider";
 import {AppRouter} from "app/providers/router";
 import {Navbar} from "widget/NavBar";
 import {Sidebar} from "widget/Sidebar";
-import {Suspense, useEffect} from "react";
+import {Suspense, useEffect, useState} from "react";
+import Modal from "shared/ui/Modal/Modal";
 
 const App = () => {
 
     const {theme} = useTheme();
 
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <div className={classNames('app', {}, [theme])}>
-
             <Suspense fallback={''}>
                 <Navbar/>
+                <button onClick={() => setIsOpen(true)}>toggle</button>
+                <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+                    Lorem imsum sdlkmsad;lask ;lkdasas; dkasq;dl kas;dl askd;
+                </Modal>
                 <div className='content-page'>
                     <Sidebar/>
                     <AppRouter/>

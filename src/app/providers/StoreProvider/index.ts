b944a894be4e2b16316c453841894f30c37dbb79
PolyakovSergey <ancientcult89@gmail.com>
2024-07-20 09:01:28 +0700

    lesson 30

---
+++ src/app/providers/StoreProvider/index.ts
@@ -0,0 +1,9 @@
+import {StoreProvider} from "./ui/StoreProvider";
+import {createReduxStore} from "app/providers/StoreProvider/config/store";
+import type {StateSchema} from './config/StateSchema'
+
+export {
+    StoreProvider,
+    createReduxStore,
+    StateSchema,
+}
